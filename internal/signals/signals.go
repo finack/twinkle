3f8c8632efa28d1a537f3acc052129eacf6790cb
Peter Baker <peter@finack.com>
2021-01-04 10:01:31 -0800

    Stop dimmer for now

--- internal/signals/signals.go
+++ internal/signals/signals.go
@@ -10,14 +10,15 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func CatchSignals(stopMetarClient chan bool, stopLedUpdate chan bool, stopApplication chan bool, stopAutomaticDimmer chan bool) {
+func CatchSignals(stopMetarClient chan bool, stopLedUpdate chan bool, stopApplication chan bool) {
+	// func CatchSignals(stopMetarClient chan bool, stopLedUpdate chan bool, stopApplication chan bool, stopAutomaticDimmer chan bool) {
 	sigs := make(chan os.Signal)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
 		s := <-sigs
 		log.Info().Str("signal", fmt.Sprintf("%v", s)).Msg("Shutting down Twinkle!")
-		stopAutomaticDimmer <- true
+		// stopAutomaticDimmer <- true
 		stopLedUpdate <- true
 		stopMetarClient <- true
 		time.Sleep(time.Millisecond * 500)
