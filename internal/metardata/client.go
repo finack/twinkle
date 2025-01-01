0f4f343ce11aa6f5de71a26f9ad9fb138e933271
Peter Baker <peter@finack.com>
2024-12-31 16:38:00 -0800

    wip

--- internal/metardata/client.go
+++ internal/metardata/client.go
@@ -19,50 +19,50 @@ import (
 
 // https://www.aviationweather.gov/dataserver/fields?datatype=metar
 type Metar struct {
-	RawText                   string  `csv:"raw_text"`                      // The raw METAR
-	StationID                 string  `csv:"station_id"`                    // Station identifier; Always a four character alphanumeric( A-Z, 0-9)
-	ObservationTime           string  `csv:"observation_time"`              // Time( in ISO8601 date/time format) this METAR was observed.
-	Latitude                  float32 `csv:"latitude"`                      // The latitude (in decimal degrees )of the station that reported this METAR
-	Longitude                 float32 `csv:"longitude"`                     // The longitude (in decimal degrees) of the station that reported this METAR
-	TempC                     float32 `csv:"temp_c"`                        // Air temperature
-	DewpointC                 float32 `csv:"dewpoint_c"`                    // Dewpoint temperature
-	WindDirDegrees            int     `csv:"wind_dir_degrees"`              // Direction from which the wind is blowing.  0 degrees=variable wind direction.
-	windSpeedKt               string  `csv:"wind_speed_kt"`                 // Wind speed; 0 degree wdir and 0 wspd = calm winds
-	windGustKt                string  `csv:"wind_gust_kt"`                  // Wind gust
-	VisibilityStatuteMi       string  `csv:"visibility_statute_mi"`         // Horizontal visibility
-	AltimInHg                 string  `csv:"altim_in_hg"`                   // Altimeter
-	SeaLevelPressureMb        string  `csv:"sea_level_pressure_mb"`         // Sea-level pressure
-	Corrected                 string  `csv:"corrected"`
-	Auto                      string  `csv:"auto"`
-	AutoStation               string  `csv:"auto_station"`
-	MaintenanceIndicatorOn    string  `csv:"maintenance_indicator_on"`
-	NoSignal                  string  `csv:"no_signal"`
-	LightningSensorOff        string  `csv:"lightning_sensor_off"`
-	FreezingRainSensorOff     string  `csv:"freezing_rain_sensor_off"`
-	PresentWeatherSensorOff   string  `csv:"present_weather_sensor_off"`
-	WxString                  string  `csv:"wx_string"`                     // wx_string descriptions
-	SkyCover                  string  `csv:"sky_cover"`
-	CloudBaseftAGL            float32 `csv:"cloud_base_ft_agl"`
-	SkyCover2                 string  `csv:"sky_cover"`
-	CloudBaseftAGL2           float32 `csv:"cloud_base_ft_agl"`
-	SkyCover3                 string  `csv:"sky_cover"`
-	CloudBaseftAGL3           float32 `csv:"cloud_base_ft_agl"`
-	SkyCover4                 string  `csv:"sky_cover"`
-	CloudBaseftAGL4           float32 `csv:"cloud_base_ft_agl"`
-	FlightCategory            string  `csv:"flight_category"`               // Flight category of this METAR. Values: VFR|MVFR|IFR|LIFR See http://www.aviationweather.gov/metar/help?page=plot#fltcat"
-	ThreeHrPressureTendencyMb float32 `csv:"three_hr_pressure_tendency_mb"` // Pressure change in the past 3 hours
-	MaxTC                     float32 `csv:"maxT_c"`                        // Maximum air temperature from the past 6 hours
-	MinTC                     float32 `csv:"minT_c"`                        // Minimum air temperature from the past 6 hours
-	MaxT24hrC                 float32 `csv:"maxT24hr_c"`                    // Maximum air temperature from the past 24 hours
-	MinT24hrC                 float32 `csv:"minT24hr_c"`                    // Minimum air temperature from the past 24 hours
-	PrecipIn                  float32 `csv:"precip_in"`                     // Liquid precipitation since the last regular METAR
-	Pcp3hrIn                  float32 `csv:"pcp3hr_in"`                     // Liquid precipitation from the past 3 hours. 0.0005 in = trace precipitation
-	Pcp6hrIn                  float32 `csv:"pcp6hr_in"`                     // Liquid precipitation from the past 6 hours. 0.0005 in = trace precipitation
-	Pcp24hrIn                 float32 `csv:"pcp24hr_in"`                    // Liquid precipitation from the past 24 hours. 0.0005 in = trace precipitation
-	SnowIn                    float32 `csv:"snow_in"`                       // Snow depth on the ground
-	VertVisFt                 int     `csv:"vert_vis_ft"`                   // Vertical Visibility
-	MetarType                 string  `csv:"metar_type"`                    // METAR or SPECI
-	ElevationM                int     `csv:"elevation_m"`                   // The elevation of the station that reported this METAR
+	RawText                   string `csv:"raw_text"`                      // The raw METAR
+	StationID                 string `csv:"station_id"`                    // Station identifier; Always a four character alphanumeric( A-Z, 0-9)
+	ObservationTime           string `csv:"observation_time"`              // Time( in ISO8601 date/time format) this METAR was observed.
+	Latitude                  string `csv:"latitude"`                      // The latitude (in decimal degrees )of the station that reported this METAR
+	Longitude                 string `csv:"longitude"`                     // The longitude (in decimal degrees) of the station that reported this METAR
+	TempC                     string `csv:"temp_c"`                        // Air temperature
+	DewpointC                 string `csv:"dewpoint_c"`                    // Dewpoint temperature
+	WindDirDegrees            string `csv:"wind_dir_degrees"`              // Direction from which the wind is blowing.  0 degrees=variable wind direction.
+	windSpeedKt               string `csv:"wind_speed_kt"`                 // Wind speed; 0 degree wdir and 0 wspd = calm winds
+	windGustKt                string `csv:"wind_gust_kt"`                  // Wind gust
+	VisibilityStatuteMi       string `csv:"visibility_statute_mi"`         // Horizontal visibility
+	AltimInHg                 string `csv:"altim_in_hg"`                   // Altimeter
+	SeaLevelPressureMb        string `csv:"sea_level_pressure_mb"`         // Sea-level pressure
+	Corrected                 string `csv:"corrected"`
+	Auto                      string `csv:"auto"`
+	AutoStation               string `csv:"auto_station"`
+	MaintenanceIndicatorOn    string `csv:"maintenance_indicator_on"`
+	NoSignal                  string `csv:"no_signal"`
+	LightningSensorOff        string `csv:"lightning_sensor_off"`
+	FreezingRainSensorOff     string `csv:"freezing_rain_sensor_off"`
+	PresentWeatherSensorOff   string `csv:"present_weather_sensor_off"`
+	WxString                  string `csv:"wx_string"`                     // wx_string descriptions
+	SkyCover                  string `csv:"sky_cover"`
+	CloudBaseftAGL            string `csv:"cloud_base_ft_agl"`
+	SkyCover2                 string `csv:"sky_cover"`
+	CloudBaseftAGL2           string `csv:"cloud_base_ft_agl"`
+	SkyCover3                 string `csv:"sky_cover"`
+	CloudBaseftAGL3           string `csv:"cloud_base_ft_agl"`
+	SkyCover4                 string `csv:"sky_cover"`
+	CloudBaseftAGL4           string `csv:"cloud_base_ft_agl"`
+	FlightCategory            string `csv:"flight_category"`               // Flight category of this METAR. Values: VFR|MVFR|IFR|LIFR See http://www.aviationweather.gov/metar/help?page=plot#fltcat"
+	ThreeHrPressureTendencyMb string `csv:"three_hr_pressure_tendency_mb"` // Pressure change in the past 3 hours
+	MaxTC                     string `csv:"maxT_c"`                        // Maximum air temperature from the past 6 hours
+	MinTC                     string `csv:"minT_c"`                        // Minimum air temperature from the past 6 hours
+	MaxT24hrC                 string `csv:"maxT24hr_c"`                    // Maximum air temperature from the past 24 hours
+	MinT24hrC                 string `csv:"minT24hr_c"`                    // Minimum air temperature from the past 24 hours
+	PrecipIn                  string `csv:"precip_in"`                     // Liquid precipitation since the last regular METAR
+	Pcp3hrIn                  string `csv:"pcp3hr_in"`                     // Liquid precipitation from the past 3 hours. 0.0005 in = trace precipitation
+	Pcp6hrIn                  string `csv:"pcp6hr_in"`                     // Liquid precipitation from the past 6 hours. 0.0005 in = trace precipitation
+	Pcp24hrIn                 string `csv:"pcp24hr_in"`                    // Liquid precipitation from the past 24 hours. 0.0005 in = trace precipitation
+	SnowIn                    string `csv:"snow_in"`                       // Snow depth on the ground
+	VertVisFt                 string `csv:"vert_vis_ft"`                   // Vertical Visibility
+	MetarType                 string `csv:"metar_type"`                    // METAR or SPECI
+	ElevationM                string `csv:"elevation_m"`                   // The elevation of the station that reported this METAR
 }
 
 func FetchRoutine(c config.Config, leds chan display.Pixel) chan bool {
